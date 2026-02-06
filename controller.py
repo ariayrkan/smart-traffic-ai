69993fe26d30b24202fd6e198f13e2f070ee63bd
ariayrkan <barisayarkan@gmail.com>
2026-02-06 11:39:41 -0500

    Update controller.py

--- controller.py
+++ controller.py
@@ -1,1 +1,6 @@
+class TrafficController:
+    def apply_decision(self, decision):
+        print(f"Traffic light action: {decision}")
 
+    def safe_mode(self):
+        print("ATTACK DETECTED: Switching to safe timed mode")
