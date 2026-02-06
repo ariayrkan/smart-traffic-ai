0bcb8e0d055a1fef21670f165bc3ac82915b048b
ariayrkan <barisayarkan@gmail.com>
2026-02-06 11:39:01 -0500

    Update ai_model.py

--- ai_model.py
+++ ai_model.py
@@ -1,1 +1,7 @@
-
+class TrafficAI:
+    def make_decision(self, data):
+        if data["emergency_vehicle"]:
+            return "GREEN_ALL"
+        if data["vehicle_count"] > 10:
+            return "EXTEND_GREEN"
+        return "NORMAL_CYCLE"
